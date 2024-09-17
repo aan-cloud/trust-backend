4b44a5eb2ca1ccc8d10c752fc1e21bc2b7946f5a
Muhammad Farhan <evirsanti@gmail.com>
2024-09-17 16:26:18 +0800

    fix: swagger

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,14 @@
 import { Hono } from "hono";
 import usersRoutes from "./routes/productsRoutes";
+import { swaggerUI } from "@hono/swagger-ui";
 
 const app = new Hono();
 
 app.get("/", async (c) => {
   return await c.html(<h1>Ini swagger nanti nya</h1>);
 });
+
+app.get("/ui", swaggerUI({url: '/doc'}))
 
 app.route("/products", usersRoutes);
 
