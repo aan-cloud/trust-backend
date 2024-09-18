44db5d5ef59a38b85f1ea7edbe01719e6d1670c4
Muhammad Farhan <evirsanti@gmail.com>
2024-09-18 16:25:48 +0800

    feat: add ui for welcome page

---
+++ src/Welcome.tsx
@@ -0,0 +1,51 @@
+export default function WelcomePage () {
+    return (
+         <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center p-4">
+      <div className="bg-white shadow-md rounded-lg p-8 max-w-4xl w-full">
+        <h1 className="text-4xl font-bold text-gray-900 mb-4 text-center">
+          Welcome to Trust API
+        </h1>
+        <p className="text-lg text-gray-700 mb-6">
+          Nusaventure helps helps you find quality automotive goods, directly from the manufacturer
+        </p>
+        <p className="text-lg text-gray-700 mb-6">
+          The OpenAPI Specification for the Trust API offers a detailed
+          description of the API's services. It can be accessed at the path{" "}
+          <code className="bg-gray-200 rounded px-2 py-1">/api-spec</code>.
+        </p>
+        <p className="text-lg text-gray-700 mb-6">
+          For a more interactive experience, you can use SwaggerUI. It provides
+          a user-friendly interface for exploring and testing the API. You can
+          access SwaggerUI at{" "}
+          <code className="bg-gray-200 rounded px-2 py-1">/ui</code>.
+        </p>
+        <p className="text-lg text-gray-700 mb-4">
+          You can also access the following:
+        </p>
+        <ul className="list-disc list-inside text-lg text-gray-700 mb-6">
+          <li>
+            <a
+              href="/api-spec"
+              target="_blank"
+              className="text-blue-500 hover:underline"
+            >
+              OpenAPI Specification
+            </a>
+          </li>
+          <li>
+            <a
+              href="/ui"
+              target="_blank"
+              className="text-blue-500 hover:underline"
+            >
+              SwaggerUI
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    )
+}
+
+
+
---
+++ src/index.tsx
@@ -0,0 +1,46 @@
+import { serve } from 'bun';
+import { swaggerUI } from "@hono/swagger-ui";
+import { registerSwaggerEndpoint } from "./config/swagger";
+import "./routes/productsRoutes";
+import { Hono } from "hono";
+import "./config/swagger";
+import WelcomePage from './Welcome';
+import productRoutes from "./routes/productsRoutes";
+
+const app = new Hono();
+
+app.get("/", async (c) => {
+  return await c.html(
+     <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Welcome to Nusaventure REST API</title>
+        <meta
+          name="description"
+          content="Trust help you find quality automotive goods, directly from the manufacturer."
+        />
+        <script src="https://cdn.tailwindcss.com"></script>
+      </head>
+      <body>
+        <WelcomePage />
+      </body>
+    </html>
+  );
+});
+
+
+registerSwaggerEndpoint(app);
+app.get("/ui", swaggerUI({url:'/api-spec'}));
+
+
+app.route('/products', productRoutes);
+
+
+const port: number = 3000;
+console.log(`Server is running on port ${port}`);
+
+serve({
+  fetch: app.fetch,
+  port
+});
