3c73cea69a637c8b22cc888e3b58c0968a57179c
Muhammad Farhan <evirsanti@gmail.com>
2024-09-20 15:56:40 +0800

    feat: add data seed product

---
+++ src/seed/products.ts
@@ -0,0 +1,56 @@
+export const data_products = [
+    {
+        "name": "Battery Charger",
+        "slug": "battery-charger",
+        "image_url": "https://ucarecdn.com/70a2a667-01b7-4d66-85e7-b0be2f0265ae/",
+        "description": "Battery chargers for all types of cars are light in weight so they can be taken anywhere",
+        "price": 2000,
+        "category": "Battery",
+        "stock": 10
+    },
+    {
+        "name": "Front Lamp",
+        "slug": "front-lamp",
+        "image_url": "https://ucarecdn.com/02014049-1920-4b8e-b064-a7be3c1490b2/",
+        "description": "Car Front Lamp",
+        "price": 1000,
+        "category": "Lamp",
+        "stock": 5
+    },
+    {
+        "name": "Cooler",
+        "slug": "cooler",
+        "image_url": "https://ucarecdn.com/5646633c-561f-4392-ab58-7328319643e5/",
+        "description": "Machine Cooler System",
+        "price": 1000,
+        "category": "cooler",
+        "stock": 5
+    },
+    {
+        "name": "Tire for toyota rush",
+        "slug": "Tire-for-toyota-rush",
+        "image_url": "https://ucarecdn.com/4d874741-3524-4523-bfeb-97d49f54907b/",
+        "description": "Toyota Tire from ring 24",
+        "price": 2500,
+        "category": "Tire",
+        "stock": 5
+    },
+    {
+        "name": "Cable de coche",
+        "slug": "cable-de-coche",
+        "image_url": "https://ucarecdn.com/be6ad6a9-12ce-4f5b-b9cc-97744f7de858/",
+        "description": "Cable de coche for toyota starlet",
+        "price": 100,
+        "category": "cable",
+        "stock": 12
+    },
+    {
+        "name": "Brake Wire",
+        "slug": "brake-wire",
+        "image_url": "https://ucarecdn.com/5b3a02d2-87de-49cc-8a55-0ef9e577d925/",
+        "description": "Brake wire for car checkout now heyyy!!",
+        "price": 400,
+        "category": "brake",
+        "stock": 10
+    }
+]
