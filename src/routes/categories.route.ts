421bdbaedccefaa65dad44c078fe1e92741e81f2
Muhammad Farhan <evirsanti@gmail.com>
2024-09-26 18:22:00 +0800

    feat: add categories route

---
+++ src/routes/categories.route.ts
@@ -0,0 +1,34 @@
+import { Hono } from "hono";
+import CategoriesServices from "../services/categories.service";
+
+const services = new CategoriesServices();
+const categoriesRoute = new Hono();
+
+categoriesRoute.get("/:category", async (c) => {
+    const category = c.req.param("category");
+    const data = await services.getCategories(category);
+
+    if (!data.length) {
+        return c.json({
+            message: "error get categories",
+            data: data
+        }, 404);
+    };
+
+    return c.json({
+        message: "succes get categories product",
+        data: data
+    }, 200);
+});
+
+categoriesRoute.get("/:slug", async (c) => {
+    const slug = c.req.param("slug");
+    const data = await services.getCategiesSlug(slug);
+
+    return c.json({
+        message: "succes get categories slug",
+        data: data
+    }, 200);
+});
+
+export default categoriesRoute;
