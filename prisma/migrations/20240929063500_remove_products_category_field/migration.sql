963d63ccc72d68bb67e0c888706bab2710201c93
Muhammad Farhan <evirsanti@gmail.com>
2024-09-30 16:10:55 +0800

    fix: erese old migration

--- prisma/migrations/20240929063500_remove_products_category_field/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `category` on the `products` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "products" DROP COLUMN "category";
