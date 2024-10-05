90123d96b3c49f55b55506a09ab103a23a60f568
Farhan <evirsanti@gmail.com>
2024-10-05 19:08:07 +0800

    refactor: change field conv name to camelcase

--- prisma/data/products.ts
+++ prisma/data/products.ts
@@ -2,7 +2,7 @@ export const dataProducts = [
   {
     name: "Battery Charger",
     slug: "battery-charger",
-    image_url: "https://ucarecdn.com/70a2a667-01b7-4d66-85e7-b0be2f0265ae/",
+    imageUrl: "https://ucarecdn.com/70a2a667-01b7-4d66-85e7-b0be2f0265ae/",
     description:
       "Battery chargers for all types of cars are light in weight so they can be taken anywhere",
     price: 2000,
@@ -12,7 +12,7 @@ export const dataProducts = [
   {
     name: "Front Lamp",
     slug: "front-lamp",
-    image_url: "https://ucarecdn.com/f5e45c85-7b49-42d2-a6d9-64b9508dcd10/",
+    imageUrl: "https://ucarecdn.com/f5e45c85-7b49-42d2-a6d9-64b9508dcd10/",
     description: "Car Front Lamp",
     price: 1000,
     category: "lamp",
@@ -21,7 +21,7 @@ export const dataProducts = [
   {
     name: "Cooler",
     slug: "cooler",
-    image_url: "https://ucarecdn.com/5646633c-561f-4392-ab58-7328319643e5/",
+    imageUrl: "https://ucarecdn.com/5646633c-561f-4392-ab58-7328319643e5/",
     description: "Machine Cooler System",
     price: 1000,
     category: "cooler",
@@ -30,7 +30,7 @@ export const dataProducts = [
   {
     name: "Tire for toyota rush",
     slug: "Tire-for-toyota-rush",
-    image_url: "https://ucarecdn.com/4d874741-3524-4523-bfeb-97d49f54907b/",
+    imageUrl: "https://ucarecdn.com/4d874741-3524-4523-bfeb-97d49f54907b/",
     description: "Toyota Tire from ring 24",
     price: 2500,
     category: "tire",
@@ -39,7 +39,7 @@ export const dataProducts = [
   {
     name: "Cable de coche",
     slug: "cable-de-coche",
-    image_url: "https://ucarecdn.com/642dedd8-e7dd-42ff-adfe-e1c066bc695f/",
+    imageUrl: "https://ucarecdn.com/642dedd8-e7dd-42ff-adfe-e1c066bc695f/",
     description: "Cable de coche for toyota starlet",
     price: 100,
     category: "cable",
@@ -48,7 +48,7 @@ export const dataProducts = [
   {
     name: "Brake Wire",
     slug: "brake-wire",
-    image_url: "https://ucarecdn.com/f4dab657-2ab8-43da-b2bd-e7a2ecf25b42/",
+    imageUrl: "https://ucarecdn.com/f4dab657-2ab8-43da-b2bd-e7a2ecf25b42/",
     description: "Brake wire for car checkout now heyyy!!",
     price: 400,
     category: "brake",
@@ -57,7 +57,7 @@ export const dataProducts = [
   {
     name: "Herness Cable",
     slug: "herness-cable",
-    image_url: "https://ucarecdn.com/520a57a8-9057-4d8f-9598-a0b037b17ba1/",
+    imageUrl: "https://ucarecdn.com/520a57a8-9057-4d8f-9598-a0b037b17ba1/",
     description: "Herness cable for your car plsss checkout",
     price: 30,
     category: "cable",
@@ -66,7 +66,7 @@ export const dataProducts = [
   {
     name: "Charger Autometer",
     slug: "charger-autometer",
-    image_url: "https://ucarecdn.com/346dec83-284d-46ae-b86b-10686b494a4c/",
+    imageUrl: "https://ucarecdn.com/346dec83-284d-46ae-b86b-10686b494a4c/",
     description: "charger autometer make your mood happy",
     price: 120,
     category: "battery",
@@ -75,7 +75,7 @@ export const dataProducts = [
   {
     name: "Blitz Cooler",
     slug: "blitz-cooler",
-    image_url: "https://ucarecdn.com/4b317b82-daa3-4d67-8f0a-bda6047823c0/",
+    imageUrl: "https://ucarecdn.com/4b317b82-daa3-4d67-8f0a-bda6047823c0/",
     description: "Blitz oil cooler you can get it",
     price: 120,
     category: "cooler",
@@ -84,7 +84,7 @@ export const dataProducts = [
   {
     name: "Mitsubitsi Tire",
     slug: "mitsubitsi-tire",
-    image_url: "https://ucarecdn.com/4d83b6d4-6127-4a2d-8e7b-b1d491437f57/",
+    imageUrl: "https://ucarecdn.com/4d83b6d4-6127-4a2d-8e7b-b1d491437f57/",
     description: "Tire for mitsubitsi",
     price: 300,
     category: "tire",
@@ -93,7 +93,7 @@ export const dataProducts = [
   {
     name: "Rush Lamp",
     slug: "rush-lamp",
-    image_url: "https://ucarecdn.com/2e9ba92c-c1ed-498c-a551-8a8e4656a96c/",
+    imageUrl: "https://ucarecdn.com/2e9ba92c-c1ed-498c-a551-8a8e4656a96c/",
     description: "Back Lamp for toyota Rush",
     price: 60,
     category: "lamp",
@@ -102,7 +102,7 @@ export const dataProducts = [
   {
     name: "Mustang AT",
     slug: "mustang-at",
-    image_url: "https://ucarecdn.com/5b55b64a-369d-463c-8c52-0121c40ce5d5/",
+    imageUrl: "https://ucarecdn.com/5b55b64a-369d-463c-8c52-0121c40ce5d5/",
     description: "Brake Tool for mustang auto transmission",
     price: 120,
     category: "brake",
