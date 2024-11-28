ad6087a49a3049bdbf0621c9f58a003dd3995cda
Farhan <evirsanti@gmail.com>
2024-11-28 12:56:45 +0800

    chore: add libraries function

---
+++ src/libs/db.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from "@prisma/client"
+import Redis from "ioredis"
+
+const prisma = new PrismaClient();
+
+export const redis = new Redis({
+  port: 6379, // Redis port
+  host: "127.0.0.1", // Redis host
+  username: "default", // needs Redis >= 6
+  password: "my-top-secret",
+  db: 4, // Defaults to 0
+});
+
+export default prisma
