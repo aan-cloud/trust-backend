b0a124c4aa4ff553f2890ee169aab334d4dc0b2f
aan-cloud <evirsanti@gmail.com>
2024-12-09 00:46:57 +0800

    fix: connect redis

--- src/libs/db.ts
+++ src/libs/db.ts
@@ -2,12 +2,18 @@ import { PrismaClient } from "@prisma/client";
 import Redis from "ioredis";
 
 const prisma = new PrismaClient();
+const redisUrl = {
+    username: process.env.REDIS_USER as string,
+    password: process.env.REDIS_PASSWORD as string,
+    host: process.env.REDIS_HOST as string,
+    port: process.env.REDIS_PORT
+};
 
 export const redis = new Redis({
-    port: 6380,
-    host: "127.0.0.1",
-    password: "my-top-secret",
-    db: 4,
+    username: redisUrl.username,
+    password: redisUrl.password,
+    host: redisUrl.host,
+    port: Number(redisUrl.port)
 });
 
 redis.ping().then((res) => {
