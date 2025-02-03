c9b85307c2694844dcde92ee04396b06daf7d6af
aan-cloud <evirsanti@gmail.com>
2025-02-03 18:57:42 +0800

    fix: include image url

--- src/services/seller.ts
+++ src/services/seller.ts
@@ -2,7 +2,10 @@ import prisma from "../libs/db";
 
 export const getSellerDashboard = async (sellerId: string) => {
     const sellerProducts = await prisma.product.findMany({
-        where: { userId: sellerId }
+        where: { userId: sellerId },
+        include: {
+            imageUrl: true
+        }
     });
 
     return sellerProducts;
