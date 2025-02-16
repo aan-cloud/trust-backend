0be447484fa436f04d360d70e983c2a2265518f0
aan-cloud <evirsanti@gmail.com>
2025-02-16 15:38:43 +0800

    fix: typo s letter

--- src/libs/oauth.ts
+++ src/libs/oauth.ts
@@ -3,7 +3,7 @@ import { google } from "googleapis";
 export const oauth2Client = new google.auth.OAuth2(
     process.env.GOOGLE_CLIENT_ID,
     process.env.GOOGLE_CLIENT_SECRET,
-    "http://trust-api.muhammad-farhan.com/auth/google/callback"
+    "https://trust-api.muhammad-farhan.com/auth/google/callback"
 );
 
 export const scopes = [
